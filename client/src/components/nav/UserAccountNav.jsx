6342e35b9669c9a527628941e743e29ace613d79
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-02-14 10:15:10 -0800

    add functions to update users theme choice

--- client/src/components/nav/UserAccountNav.jsx
+++ client/src/components/nav/UserAccountNav.jsx
@@ -1,15 +1,37 @@
+import axios from "axios"
+import { useContext } from "react"
 import { logout } from "../../lib/utils"
 import { UserAuthBtn } from "../buttons/UserAuthBtn"
+import { UserContext } from "../context/User"
+import { Link } from "react-router-dom"
 
 export const UserAccountNav = (props) => {
+  const { user, setUser } = useContext(UserContext)
   const toggleTheme = () => {
     let rootElemment = document.getElementById("root")
-
     if (rootElemment.classList.contains("dark-theme")) {
       rootElemment.classList.remove("dark-theme")
     } else {
       rootElemment.classList.add("dark-theme")
     }
+    const data = {
+      id: user.id,
+      isDarkTheme: !user.isDarkTheme
+    }
+    axios({
+      url: "http://localhost:5000/api/users/theme",
+      method: "POST",
+      data: data,
+    }).then(function (response) {
+      if (response.status === 200) {
+        let updatedUser = {...user, isDarkTheme: !user.isDarkTheme}
+        setUser(updatedUser)
+      }
+      return 
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
   const logOutUser = () => {
     props.setIsAccountNavOpen(false)
@@ -21,9 +43,9 @@ export const UserAccountNav = (props) => {
   }
 
   return (
-    <ul>
+    <ul onMouseLeave={closeMenu}>
         <li>
-          <a tabIndex={props.isShown ? 0 : -1} className="link underline" href="">Profile</a>
+          <Link tabIndex={props.isShown ? 0 : -1} className="link underline" to="/settings/profile">Profile</Link>
         </li>
         <ul>
           <li>
@@ -33,10 +55,10 @@ export const UserAccountNav = (props) => {
           </li>
         </ul>
         <li>
-          <a tabIndex={props.isShown ? 0 : -1} className="link underline" href="">Settings</a>
+          <Link tabIndex={props.isShown ? 0 : -1} className="link underline" to="/settings/account">Settings</Link>
         </li>
         <li>
-          <UserAuthBtn isShown={props.isShown} onBlurEvent={closeMenu} onClickEvent={logOutUser} dataType="accent" text="log out"/>
+          <UserAuthBtn isShown={props.isShown} onBlurEvent={closeMenu} onClickEvent={logOutUser} dataType="primary" text="log out"/>
         </li>
     </ul>
   )
