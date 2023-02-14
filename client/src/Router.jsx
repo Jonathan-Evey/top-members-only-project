073c63b2ce86972784182be4a69a7a1219dfb514
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-02-14 10:09:47 -0800

    add settings route to the router

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,16 +3,18 @@ import { HomePage } from './pages/home/index'
 import { AboutPage } from './pages/about/index'
 import { ContactPage } from './pages/contact/index'
 import { ArticleRoutes } from './pages/articles/router'
+import { SettingRoutes } from './pages/settings/router'
 import { ErrorPage } from './pages/error/index'
 
 const Router = () => {
   return (
         <Routes>
-            <Route path='/' element={<HomePage />}/>
-            <Route path='/about' element={<AboutPage />}/>
-            <Route path='/contact' element={<ContactPage />}/>
-            <Route path='/articles/*' element={<ArticleRoutes />}/>
-            <Route path='*' element={<ErrorPage />}/>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/contact' element={<ContactPage />} />
+            <Route path='/articles/*' element={<ArticleRoutes />} />
+            <Route path='/settings/*' element={<SettingRoutes />} />
+            <Route path='*' element={<ErrorPage />} />
         </Routes>
   )
 }
