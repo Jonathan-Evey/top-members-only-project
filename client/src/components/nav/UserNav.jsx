c7f4deb812f83112ddff8f76919d69b3774ffc05
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-02-14 10:16:28 -0800

    add modal for login and register events

--- client/src/components/nav/UserNav.jsx
+++ client/src/components/nav/UserNav.jsx
@@ -3,11 +3,13 @@ import { UserContext } from "../context/User"
 import { UserAuthBtn } from "../buttons/UserAuthBtn"
 import { UserAccountNav } from  './UserAccountNav'
 import { UserAccountNavBtn } from "../buttons/UserAccountNavBtn"
+import { UserAuthModal } from "../modals/UserAuthModal"
 
 export const UserNav = () => {
     const { user, setUser } = useContext(UserContext)
     
     const [isAccountNavOpen, setIsAccountNavOpen] = useState(false)
+    const [modalForm, setModalForm] = useState("login")
 
     const removeClass = () => {
         setTimeout(() => {
@@ -16,12 +18,34 @@ export const UserNav = () => {
         },250)
     }
 
+    const openModal = () => {
+        let modal = document.querySelector(".modal__user-auth")
+        modal.classList.remove("open")
+        modal.showModal()
+        modal.classList.add("open")
+    }
+
+    const openLoginModal = () => {
+        setModalForm("login")
+        openModal()
+    }
+
+    const openRegisterModal = () => {
+        setModalForm("register")
+        openModal()
+    }
+
     useEffect(() => {
         if (user) {
             let userNav = document.querySelector(".nav__user-account")
             if (userNav.classList.contains("display-none")) {
                 removeClass()
-            } 
+            }
+            if (user.isDarkTheme) {
+                let rootElemment =  document.getElementById("root")
+                rootElemment.classList.add("dark-theme")
+            }
+            
         } 
     })
   return (
@@ -29,16 +53,17 @@ export const UserNav = () => {
         {!user ? (
         <ul className="flex-group">
             <li>
-                <UserAuthBtn dataType="light" text="Login"/>
+                <UserAuthBtn onClickEvent={openLoginModal} dataType="light" text="Login"/>
             </li>
             <li>
-                <UserAuthBtn dataType="accent" text="Sign Up"/>
-            </li> 
+                <UserAuthBtn onClickEvent={openRegisterModal} dataType="accent" text="Sign Up"/>
+            </li>
+            <UserAuthModal modalForm={modalForm}/>
                 </ul>) 
             : 
         <ul>    
             <li>
-                <UserAccountNavBtn isAccountNavOpen={isAccountNavOpen}  setIsAccountNavOpen={setIsAccountNavOpen}/>
+                <UserAccountNavBtn user={user} isAccountNavOpen={isAccountNavOpen}  setIsAccountNavOpen={setIsAccountNavOpen}/>
             </li>
             <li aria-hidden={!isAccountNavOpen ? "true" : "false"} className="nav__user-account display-none">
                 <UserAccountNav setIsAccountNavOpen={setIsAccountNavOpen} setUser={setUser} isShown={isAccountNavOpen} /> 
