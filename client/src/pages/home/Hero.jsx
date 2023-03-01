6e9f5c8933f9c24931836283f9d67f43f05e4ac0
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-03-01 08:58:11 -0800

    add a check for if a user is loged in, and will changed the display. Also adds in login and sign up

--- client/src/pages/home/Hero.jsx
+++ client/src/pages/home/Hero.jsx
@@ -1,5 +1,35 @@
-export const Hero = () => {
+import { UserAuthBtn } from "../../components/buttons/UserAuthBtn"
+import { useContext } from "react"
+import { AuthModalContext } from "../../components/context/AuthModal"
+
+export const Hero = (props) => {
+  const { openRegisterModal, openLoginModal } = useContext(AuthModalContext)
+
+
+  if (!props.user) {
+    return (
+      <section className="hero | container section padding-inline-4" data-type="wide">
+      <div>
+        <h1>Welcome to The Accessible Collective</h1>
+        <p>Whether you are a user of assistive tools and technologies, a Developer working towards making your applications more accessible, or just interested in web accessibility. This is a place to share and learn with others about accessibility on the web.</p>
+      </div>
+      <div className="flex">
+        <UserAuthBtn onClickEvent={openRegisterModal} dataType="accent" text="Sign Up"/>
+        <UserAuthBtn onClickEvent={openLoginModal} dataType="light" text="Login"/>
+      </div>
+    </section>
+    )
+  }
+
   return (
-    <div>Hero</div>
+    <section className="hero | container section padding-inline-4" data-type="wide">
+    <div>
+      <h1>Welcome Back, {props.user.userName}!</h1>
+
+    </div>
+    <div>
+
+    </div>
+  </section>
   )
 }
