f7a6922ad566a7dd1f70daf6c4ddbe9b3d967f16
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-08-30 09:55:41 -0700

    remove props from modal component, and inport state context from authmoalcontext

--- client/src/components/modals/UserAuthModal.jsx
+++ client/src/components/modals/UserAuthModal.jsx
@@ -1,8 +1,13 @@
 import { Button } from "../buttons/Button"
 import { LoginUser } from "../form/LoginUser"
 import { RegisterUser } from "../form/RegisterUser"
+import { AuthModalContext } from "../context/AuthModal"
+import { useContext } from "react"
+
 
 export const UserAuthModal = (props) => {
+  const { modalForm, setModalForm } = useContext(AuthModalContext)
+
     const handleEvent = (e) => {
         if (e.target.classList.contains("modal__user-auth")) {
             e.target.close()
@@ -18,8 +23,8 @@ export const UserAuthModal = (props) => {
     <dialog className="modal__user-auth"  onMouseDown={(e) => {handleEvent(e)}}>
         <div>
           <Button btnStyle="xButton" class="btn__close" clickEvent={closeModal}/>
-          <h2>{props.modalForm === "login" ? "Login" : "Create an Account" }</h2>
-          {props.modalForm === "login" ? 
+          <h2>{modalForm === "login" ? "Login" : "Create an Account" }</h2>
+          {modalForm === "login" ? 
           <LoginUser /> :
           <RegisterUser /> }
         </div>
