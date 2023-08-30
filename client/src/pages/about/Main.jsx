63c7cf3d72d99df47371848af96f720a87ac5b8f
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-08-30 09:59:40 -0700

    start of the main component for the about page

---
+++ client/src/pages/about/Main.jsx
@@ -0,0 +1,24 @@
+export const Main = () => {
+  return (
+    <main id="main">
+        <header>
+            <h1>
+                Our Mission
+            </h1>
+        </header>
+
+        <section>
+          <header>
+           <h2>Our Team</h2> 
+          </header>
+
+            <section>
+              <h3>
+                Jonathan Evey
+              </h3>
+              <p>Founder and Designer</p>
+            </section>
+        </section>
+    </main>
+  )
+}
