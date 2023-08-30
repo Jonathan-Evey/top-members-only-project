ed2a05c52cee16a1958272292f13bf412b5b1f13
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-08-30 09:53:25 -0700

    adds new site name, and logo

--- client/src/components/header/SiteHeader.jsx
+++ client/src/components/header/SiteHeader.jsx
@@ -5,9 +5,12 @@ export const SiteHeader = () => {
   return (
     <header className="header__site">
       <div className="header__container | padding-inline-6"> 
-        <div className="flex-group">
-          <div>AC</div>
-          <h1>Accessible<br/> Collective</h1>
+        <div className="flex-group | center">
+          <div className="logo"><img src="/img/logos/texas-map-4.svg"></img></div>
+          <div className="padding-inline-start-2">
+            <p className="site-name | ff-logo">Panhandle</p>
+            <p className="site-name | ff-logo margin-inline-start-2">Pruning</p>
+          </div>
         </div>
         <UserNav />
       </div>
